92604f3178db8b60af19023939b99f0bd75c9e34
Gleb Nikiforov <nikiforov02@list.ru>
2022-03-31 16:40:19 +0300

    Задание 6 (5)

--- lr65.py
+++ lr65.py
@@ -1,17 +1,30 @@
+def GetWordList():
+    return [str(word) for word in input("Enter word list: ").split(', ')]
 
+def WordListToSet(wordList):
+    wordSet = set(wordList)
+    wordSetLength = len(wordSet)
+    print("This word set cotains {0} word(s).".format(wordSetLength))
+    return wordSet
 
-wordList = [str(word) for word in input("Enter word list: ").split(', ')]
-wordSet = set(wordList)
-wordSetLength = len(wordSet)
-print("This word set cotains {0} word(s).".format(wordSetLength))
+def GetValueList(length):
+    print("Enter value list with {0} elements: ".format(length))
+    valueList = list()
+    for i in range(0, length, 1):
+        valueList.append(input("Enter {0}-th value: ".format(i)))
+    return valueList
 
-print("Enter second list with {0} elements: ".format(wordSetLength))
-valueList = list()
-for i in range(0, wordSetLength, 1):
-    valueList.append(input("Enter {0}-th value: ".format(i)))
+def CreateDictionary(words, values):
+    dictionary = {}
+    for word, value in zip(words, values):
+        dictionary.setdefault(word, value)
+    return dictionary
 
-dictionary = {}
-for word, value in zip(wordSet, valueList):
-    dictionary.setdefault(word, value)
+def main():
+    wordList = GetWordList()
+    wordSet = WordListToSet(wordList)
+    valueList = GetValueList(len(wordSet))
+    createdDictionary = CreateDictionary(wordSet, valueList)
+    print("Created dictionary: {0}".format(createdDictionary))
 
-print("Created dictionary: {0}".format(dictionary))
+main()
